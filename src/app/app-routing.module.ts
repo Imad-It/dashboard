5b2f7264cbcc97ec349d3a03a51d1f57473d1c62
Imad-It <itgeek14@gmail.com>
2022-04-16 22:45:07 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ChartsComponent } from './components/charts/charts.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'charts', component: ChartsComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { TopbarComponent } from './components/topbar/topbar.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { CustomersComponent } from './components/customers/customers.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarChartsComponent } from './components/charts/bar-charts/bar-charts.component';
+import { PieChartsComponent } from './components/charts/pie-charts/pie-charts.component';
 
 
 
@@ -18,10 +24,15 @@ import { CustomersComponent } from './components/customers/customers.component';
     TopbarComponent,
     CardsComponent,
     OrdersComponent,
-    CustomersComponent
+    CustomersComponent,
+    DashboardComponent,
+    ChartsComponent,
+    BarChartsComponent,
+    PieChartsComponent
   ],
   imports: [
     BrowserModule,
+    NgxChartsModule, BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
